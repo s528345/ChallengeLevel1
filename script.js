af5f68b9a3453d038b18461d3e9352f61e85f03e
s528345 <42850453+s528345@users.noreply.github.com>
2020-03-02 10:45:05 -0600

    Commit 3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function namelength() 
     { 
-        const string = document.getElementById("length");
+        const string = document.getElementById("name");
         const length = string.length;
         return answer.innerHTML = `Number of Letters: ${length}`;
     }
